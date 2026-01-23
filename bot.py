60f0c06c7d930542fe09aa8a3b25c41e89c21a52
sumbarine13 <sumbarine13@gmail.com>
2026-01-23 21:09:00 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 from datetime import datetime, timedelta
 import asyncio
+from discord import app_commands
 
 # =========================
 # GROQ SETUP
